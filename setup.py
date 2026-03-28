013ead950d3181a30a5edb9d1f5931003ff405dd
chinmoy31112 <chinmoysaha31112@gmail.com>
2026-03-28 18:08:56 +0530

    git error fixed

--- setup.py
+++ setup.py
@@ -24,4 +24,9 @@ setuptools.setup(
     },
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
+    install_requires=[
+        "python-box",
+        "ensure",
+        "pyYAML"
+    ]
 )
